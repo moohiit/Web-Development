3a6b3c5457ef2165fcc8ced71fce102afcd58197
moohiit <pmohit645@gmail.com>
2023-09-15 19:21:51 +0530

    new files added

---
+++ JavaScript-Full-Course/03_Basics/02_Scope.js
@@ -0,0 +1,50 @@
+//var c = 300
+// let a = 300;
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//   console.log("INNER: ", a);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one() {
+//     const username = "hitesh";
+
+//     two();
+//     }
+// function two() {
+//     const website = "youtube";
+//     console.log(username);
+// }
+// console.log(website);
+
+
+// one()
+
+// if (true) {
+//   const username = "hitesh";
+//   if (username === "hitesh") {
+//     const website = " youtube";
+//     console.log(username + website);
+//   }
+// //   console.log(website);
+// }
+
+// console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+// console.log(addone(5));
+
+// function addone(num) {
+//   return num + 1;
+// }
+
+// const addTwo = function (num) {
+//     return num + 2;
+// };
+
+// addTwo(5);
